aa98b419f98b1d5d9051907b6cda1a26377a8a14
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2025-02-08 13:14:16 +0100

    ""

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,11 +6,12 @@
 /*   By: zajaddad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 18:00:10 by zajaddad          #+#    #+#             */
-/*   Updated: 2025/01/26 02:26:29 by zajaddad         ###   ########.fr       */
+/*   Updated: 2025/02/07 19:16:36 by zajaddad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+#include <cstdio>
 #include <stdio.h>
 
 /*
@@ -70,7 +71,7 @@ int	count_elements(char **split_elements)
 	return (counter);
 }
 
-int	*make_arr(char **elements)
+int	*make_arr(char **elements, unsigned int *arr_size)
 {
 	int	*arr;
 	int	*arr_head;
@@ -79,21 +80,37 @@ int	*make_arr(char **elements)
 	arr_head = arr;
 	if (arr == NULL)
 		return (NULL);
+	*arr_size = 0;
 	while (*elements)
 	{
 		if (ft_atoi(*elements) == ATOIERROR || (ft_atoi(*elements) < INT_MIN
 				|| ft_atoi(*elements) > INT_MAX))
-			return (free((void *) arr), arr = NULL);
+			return (free(arr_head), *arr_size = 0, arr = NULL);
 		*arr = (int) ft_atoi(*elements);
-		(arr++, elements++);
+		(arr++, elements++, (*arr_size)++);
 	}
 	return (arr_head);
 }
 
-int	*parse_elements(char *elements)
+/*
+ * check_duplication: 
+ * duplicate number found 	-> 1
+ * duplicate number not found 	-> 0
+ */
+int	check_duplication(int *arr, unsigned int arr_size) 
+{
+	/* int i; */
+	/* int j; */
+	/* i = 0; */
+	(void) arr;
+	return (0);
+}
+
+t_stack *parse_elements(char *elements)
 {
 	char	**split_elements;
 	int *arr;
+	unsigned int arr_size;
 
 	// split elements based on spaces
 	split_elements = ft_split(elements, ' ');
@@ -106,28 +123,35 @@ int	*parse_elements(char *elements)
 		return (free_args(split_elements), split_elements = NULL, NULL);
 
 	// convert elements to a list of integers
-	arr = make_arr(split_elements);
-	printf("arrr %s\n", (char *) arr);
+	arr = make_arr(split_elements, &arr_size);
 	if (arr == NULL)
 		return (free_args(split_elements), split_elements = NULL, NULL);
+	(void) (free_args(split_elements), split_elements = NULL);
 
-	for (int i = 0; i < count_elements(split_elements); i++)
-		printf("%d\n", arr[i]);
-
-	return ((int *)split_elements);
+	// check duplication in the list
+	if (check_duplication(arr, arr_size) == 1)
+		return (free(arr), arr = NULL, NULL);
+	
+	return ((t_stack *)arr);
 }
+
 
 /* t_stack *stack_a = create_stack(--argc, ++argv); */
 int	main(int argc, char **argv)
 {
 	char	*elements;
 	int		*arr;
+	unsigned int	arr_size;
 
 	elements = join_arguments(--argc, ++argv);
 	if (elements == NULL)
 		prog_error("Error\n");
 	arr = parse_elements(elements);
-	(void)arr;
+	if (arr == NULL)
+		prog_error("Error\n");
+
+	// check duplication
+	/* if (check_duplication(arr) == 1) */
 	/* ft_printf("%s", elements); */
 	/* free(elements); */
 	return (EXIT_SUCCESS);
