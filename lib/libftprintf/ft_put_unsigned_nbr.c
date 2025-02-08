0351c2f51111832fc49cbb89b3e4d17794eca638
Zakaria Jaddad <zakaria.jaddad26@gmail.com>
2025-02-08 13:28:58 +0100

    Fixed put_unsigned in linux compiler

--- lib/libftprintf/ft_put_unsigned_nbr.c
+++ lib/libftprintf/ft_put_unsigned_nbr.c
@@ -6,14 +6,14 @@
 /*   By: zajaddad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 22:18:15 by zajaddad          #+#    #+#             */
-/*   Updated: 2024/11/17 17:44:16 by zajaddad         ###   ########.fr       */
+/*   Updated: 2025/02/08 13:28:18 by zajaddad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
 void	ft_put_unsigned_nbr(unsigned int n, int *counter)
 {
-	if (n >= 0 && n < 10)
+	if (n < 10)
 		*counter += ft_putchar((n + '0'));
 	else
 	{
